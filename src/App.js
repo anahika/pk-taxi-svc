e5b9bc69aa0c870975e0da4dd972bf7a1929f25d
Akash Rana <akashrana20022000@gmail.com>
2023-05-07 13:56:11 +0530

    added BrowserRouter for CSR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import AboutUs from "./components/about-us/AboutUs";
 import Landing from "./components/landing/Landing";
 import ContactUs from "./components/contact-us/ContactUs";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Error } from "./components/pages/Error";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';import { Error } from "./components/pages/Error";
 import Header from "./components/header/Header";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <Header />
         <Routes>
           <Route exact path="/" element={<Landing />} />
-          <Route exact path="/contact-us/" element={<ContactUs />} />
           <Route exact path="/about-us" element={<AboutUs />} />
-          <Route path="/*" element={<Error />} />
+          <Route exact path="/contact-us" element={<ContactUs />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
