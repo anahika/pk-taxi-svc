076998bc494596b5453b103a2834b58521ab4bbd
Akash Rana <akashrana20022000@gmail.com>
2024-08-31 13:24:16 +0530

    renamed back to TravelTheoriz

--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Section from "./Section";
-import "./AboutUs.css";
+import "./aboutUs.css";
 
 function AboutUs() {
   return (
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 import AboutUs from "../about-us/AboutUs";
 import ContactUs from "../contact-us/ContactUs";
 import Carousal from "../carousal/Carousel";
-import './Landing.css'
+import './landing.css'
 
 function Landing() {
   const phoneNumber = 8803660003;
@@ -14,7 +14,7 @@ function Landing() {
             <Carousal />
           </div>
           <div className="col-md-6 d-flex flex-column justify-content-center align-items-center mt-5">
-            <h1 className="text-warning">Welcome to Prashant Travels</h1>
+            <h1 className="text-warning">Welcome to Travel Theoriz</h1>
             <h1>It's time to travel</h1>
             <a className="contact-button" href={`tel:${phoneNumber}`}>
               CONTACT US
@@ -26,7 +26,7 @@ function Landing() {
       <ContactUs />
       <footer className="text-center text-black-50 p-3">
         {" "}
-        <span>COPYRIGHT © 2023 PRASHANT TRAVELS - ALL RIGHTS RESERVED.</span>
+        <span>COPYRIGHT © 2024 TRAVEL THEORIZ - ALL RIGHTS RESERVED.</span>
       </footer>
     </div>
   );
